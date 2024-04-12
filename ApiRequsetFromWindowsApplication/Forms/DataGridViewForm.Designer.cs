736c67566871710e020e8abb0faf701d3b463cc8
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2024-04-12 07:51:55 +0530

    add img column into grid view

--- ApiRequsetFromWindowsApplication/Forms/DataGridViewForm.Designer.cs
+++ ApiRequsetFromWindowsApplication/Forms/DataGridViewForm.Designer.cs
@@ -34,21 +34,26 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 97);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.Size = new System.Drawing.Size(776, 267);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
-            // DataGridView
+            // DataGridViewForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.dataGridView1);
-            this.Name = "DataGridView";
+            this.Name = "DataGridViewForm";
             this.Text = "DataGridView";
             this.Load += new System.EventHandler(this.DataGridView_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
--- ApiRequsetFromWindowsApplication/Forms/DataGridViewForm.cs
+++ ApiRequsetFromWindowsApplication/Forms/DataGridViewForm.cs
@@ -23,17 +23,66 @@ namespace ApiRequsetFromWindowsApplication.Forms
             dataGridView1.Columns[0].Name = "Name"; 
             dataGridView1.Columns[1].Name = "Address";
             dataGridView1.Columns[2].Name = "Gender";
+            
+            // Add Button Column
+            //DataGridViewButtonColumn btn = new DataGridViewButtonColumn();
+            //btn.HeaderText = "Action";
+            //btn.Text = "Print";
+            //btn.Name = "btn";
+            //btn.DefaultCellStyle.BackColor = SystemColors.Control;
+            //btn.DefaultCellStyle.ForeColor = SystemColors.Control;
+            //btn.UseColumnTextForButtonValue = true;
+            //btn.FlatStyle = FlatStyle.System;
+            //dataGridView1.Columns.Add(btn);
 
-            DataGridViewButtonColumn btn = new DataGridViewButtonColumn();
-            btn.HeaderText = "Action";
-            btn.Text = "Print";
-            btn.Name = "btn";
-            btn.UseColumnTextForButtonValue = true;
-            btn.FlatStyle = FlatStyle.System;
-            dataGridView1.Columns.Add(btn);
 
-            string[] row = new string[] { "Dilshan", "Matara", "Male" };
+            // Add Image Column
+            Image img = Image.FromFile("C:/Z - MY DATA/- My Projects/.Net/ApiRequsetFromWindowsApplication/ApiRequsetFromWindowsApplication/Assets/icon.png");
+
+            DataGridViewImageColumn imgCol = new DataGridViewImageColumn();
+            imgCol.Name = "Action";
+            imgCol.HeaderText = "Action/Print";
+            imgCol.Image = img;
+            imgCol.ImageLayout = DataGridViewImageCellLayout.Zoom;
+
+            dataGridView1.Columns.Add(imgCol);
+
+            // Add data into data grid
+            string[] row = new string[] { "Dilshan1", "Matara1", "Male" };
+            string[] row2 = new string[] { "Dilshan2", "Matara2", "Male" };
+            string[] row3 = new string[] { "Dilshan3", "Matara3", "Male" };
             dataGridView1.Rows.Add(row);
+            dataGridView1.Rows.Add(row2);
+            dataGridView1.Rows.Add(row3);
+
+            // Disable the last blank line in DatagridView
+            dataGridView1.AllowUserToAddRows = false;
+
+
+        }
+
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int selectedRowIndex = e.RowIndex;
+            int selectedColumnIndex = e.ColumnIndex;
+
+            if (selectedRowIndex >= 0 && selectedColumnIndex >= 0)
+            {
+                // Get the column of the selected cell
+                DataGridViewColumn selectedColumn = dataGridView1.Columns[selectedColumnIndex];
+                // Get the column name or header text
+                string actionColumnName = selectedColumn.Name;
+
+                if (actionColumnName.ToString() == "Action") 
+                {
+                    DataGridViewRow selectedRow = dataGridView1.Rows[selectedRowIndex];
+                    var cellCount = selectedRow.Cells.Count;
+                    var dataCellIndex0 = selectedRow.Cells[0].Value.ToString();
+
+                    MessageBox.Show($"{dataCellIndex0}");
+                }
+            }
         }
     }
 }
